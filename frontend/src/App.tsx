c2cd1266aa235a47ceaf9df3044b7b436a82a804
Kevin Carter <console.logkevin@gmail.com>
2024-01-10 06:42:22 -0500

    finish init project setup for layout and consistent components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,20 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
-        <Route path="/search" element={<>Search Page</>} />
+        <Route 
+          path="/" 
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          } />
+        <Route 
+          path="/search" 
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          } />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
