0c1f74f005cbe95441067279ec3bfb34397fcacc
Kevin Carter <console.logkevin@gmail.com>
2024-01-11 11:04:19 -0500

    add auth rout and validation

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -11,5 +13,15 @@ router.post("/login", [
    if (!errors.isEmpty()) res.sendStatus(400).json({ messgae: errors.array() });
    
    const { email, password } = req.body;
-   
+
+   try {
+      const user = await User.findOne({ email })
+      if (!user) res.status(400).json({ message: "Invalid credentials" });
+      
+      const isMatch = bcrypt.compare(password, user.password)
+      
+   } catch(error) {
+      console.log(error);
+      res.status(500).json({ message: "Somthng went wrong" });
+   }
 })
